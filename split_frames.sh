179e90701a836b7844a33b01564f5f5c497dcfaa
Dom De Felice <dom@domdefelice.net>
2018-11-04 11:47:01 +0000

    Added comments to split_frames.sh

--- split_frames.sh
+++ split_frames.sh
@@ -1,1 +1,4 @@
+# Extract frames in JPEG format from the video. The -r option determines the number of frames per second
 ffmpeg -i ../../video2.mp4 -r 2 %04d.jpg
+# Optional: downsample frames to a lower resolution for better performance of the classification model
+ffmpeg -i video2frames/%04d.jpg -vf scale=720x480 video2framesdownsample/%04d.jpg
