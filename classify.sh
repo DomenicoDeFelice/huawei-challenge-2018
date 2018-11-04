8dcf5272ed725628e55b69f3b50d560596d9d151
Dom De Felice <dom@domdefelice.net>
2018-11-04 10:38:30 +0000

    Remaining scripts and second video csv

---
+++ classify.sh
@@ -0,0 +1,3 @@
+# 1st argument: directory with pictures of the person to be recognized
+# 2nd argument: directory containing the frames to be classified
+face_recognition /mnt/dom/harry /mnt/dom/video2framesdownsample
---
+++ split_frames.sh
@@ -0,0 +1,1 @@
+ffmpeg -i ../../video2.mp4 -r 2 %04d.jpg
